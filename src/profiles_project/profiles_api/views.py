366353b8901ac01dadb95eca65054c015c62a225
Mohammad Sayed <masayed@Mohammads-MBP.home>
2019-04-15 22:11:24 +0300

    Created POST function

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+
 # Create your views here.
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = [
                 'Uses HTTP methods as function(get, post, patch,put,delete)',
@@ -12,3 +18,11 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        def post(self, request):
+            serializer = serializers.HelloSerializer(data=request.data)
+            if serializer.is_valid():
+                name = serializer.data.get('name')
+                message = 'Hello {0}'.format(name)
+                return Response({'message': message})
+            else:
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
